4eba4f5d63ff862ce19264465c2e1104b58344ae
steven-terrana <steventerrana@gmail.com>
2019-05-07 16:08:40 -0400

    update with working collapse

--- src/main/resources/org/boozallen/plugins/jte/console/TemplateLogger/script.js
+++ src/main/resources/org/boozallen/plugins/jte/console/TemplateLogger/script.js
@@ -6,22 +6,38 @@ document.addEventListener('DOMContentLoaded', function() {
 }, false);
 
 function jteCollapse(link){
+    console.log("inside jteCollapse()")
     var span = link.parentNode;
     var isHidden = link.getAttribute("isHidden"); 
     var display; 
-    if (isHidden == 'true'){
+    console.log("isHidden = " + isHidden);
+    // if (isHidden === 'true'){
+    //     console.log("text was hidden");
+    //     link.textContent = "(show)";
+    //     link.style.visibility = "visible"; 
+    //     link.setAttribute("isHidden", 'false');
+    //     display = "none";
+    // }else{
+    //     console.log("text was visible");
+    //     link.textContent = "(hide)";
+    //     link.setAttribute("isHidden",'true');
+    //     link.style.visibility = "";
+    //     display = "inline"; 
+    // }
+
+    if(isHidden === 'false'){
+        link.setAttribute("isHidden",'true');
+        display = 'none';
         link.textContent = "(show)";
         link.style.visibility = "visible"; 
-        link.setAttribute("isHidden", "false")
-        display = "none";
     }else{
-        link.textContent = "(hide)" 
-        link.setAttribute("isHidden","true")
-        link.style.visibility = "";
-        display = "inline"; 
+        link.setAttribute("isHidden",'false');
+        display = 'inline';
+        link.textContent = "(hide)";
+        link.style.visibility = ""; 
     }
     var id = span.getAttribute("jte-id");
-    
+    console.log("setting display to " + display);
     document.querySelectorAll(`span[jte-id='${id}'][first-line='false']`).forEach( (element) => {
         element.style.display = display;
     });
