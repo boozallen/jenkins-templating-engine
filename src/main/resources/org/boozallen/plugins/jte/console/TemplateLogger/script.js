f38255bd2baab15b3eb271166d9cfb31fb48373c
steven-terrana <steventerrana@gmail.com>
2019-05-08 09:17:25 -0400

    update log message id to be random string

--- src/main/resources/org/boozallen/plugins/jte/console/TemplateLogger/script.js
+++ src/main/resources/org/boozallen/plugins/jte/console/TemplateLogger/script.js
@@ -6,25 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
 }, false);
 
 function jteCollapse(link){
-    console.log("inside jteCollapse()")
     var span = link.parentNode;
     var isHidden = link.getAttribute("isHidden"); 
     var display; 
-    console.log("isHidden = " + isHidden);
-    // if (isHidden === 'true'){
-    //     console.log("text was hidden");
-    //     link.textContent = "(show)";
-    //     link.style.visibility = "visible"; 
-    //     link.setAttribute("isHidden", 'false');
-    //     display = "none";
-    // }else{
-    //     console.log("text was visible");
-    //     link.textContent = "(hide)";
-    //     link.setAttribute("isHidden",'true');
-    //     link.style.visibility = "";
-    //     display = "inline"; 
-    // }
-
     if(isHidden === 'false'){
         link.setAttribute("isHidden",'true');
         display = 'none';
@@ -37,7 +21,6 @@ function jteCollapse(link){
         link.style.visibility = ""; 
     }
     var id = span.getAttribute("jte-id");
-    console.log("setting display to " + display);
     document.querySelectorAll(`span[jte-id='${id}'][first-line='false']`).forEach( (element) => {
         element.style.display = display;
     });
