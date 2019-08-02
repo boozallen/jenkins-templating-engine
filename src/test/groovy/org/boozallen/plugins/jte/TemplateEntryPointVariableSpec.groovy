85c4d1f164945e0892d5913f9d042b96a2127c03
Carlos OKieffe <okieffe_carlos@bah.com>
2019-08-02 14:13:18 -0400

    added aggregateConfigurations tests

--- src/test/groovy/org/boozallen/plugins/jte/TemplateEntryPointVariableSpec.groovy
+++ src/test/groovy/org/boozallen/plugins/jte/TemplateEntryPointVariableSpec.groovy
@@ -1,8 +1,11 @@
 package org.boozallen.plugins.jte
 
 import org.boozallen.plugins.jte.binding.TemplateBinding
+import org.boozallen.plugins.jte.config.GovernanceTier
 import org.boozallen.plugins.jte.config.PipelineConfig
+import org.boozallen.plugins.jte.config.TemplateConfigDsl
 import org.boozallen.plugins.jte.config.TemplateConfigObject
+import org.boozallen.plugins.jte.utils.FileSystemWrapper
 import org.boozallen.plugins.jte.utils.RunUtils
 import org.boozallen.plugins.jte.utils.TemplateScriptEngine
 import org.jenkinsci.plugins.workflow.cps.CpsScript
@@ -100,6 +103,77 @@ class TemplateEntryPointVariableSpec extends Specification {
 
     then:
     value == parseResult
+
+  }
+
+  def "aggregateTemplateConfigurations 1 tier, no local config"(){
+    setup:
+    PipelineConfig pipelineConfigMain =  Mock(PipelineConfig)
+
+    TemplateConfigObject templateConfigObject = Mock(TemplateConfigObject)
+    //1 * templateConfigObject.asBoolean() >> { return true }
+
+    1 * pipelineConfigMain.join(templateConfigObject) >> { return }
+
+    GovernanceTier tier = GroovyMock(GovernanceTier, global: true)
+    tier.getConfig() >> { return templateConfigObject }
+
+    1 * GovernanceTier.getHierarchy() >> { return [ tier ] }
+    1 * GovernanceTier.getCONFIG_FILE() >> { return "pipeline_config.groovy" } // mocks GovernanceTier.CONFIG_FILE
+
+    FileSystemWrapper fsw = GroovyMock(FileSystemWrapper, global: true)
+    1 * FileSystemWrapper.createFromJob() >> { return fsw }
+    1 * fsw.getFileContents(_, _, _) >> { return null }
+
+
+    when:
+    templateEntryPointVariable.aggregateTemplateConfigurations(pipelineConfigMain)
+
+    then:
+    true == true
+  }
+
+  def "aggregateTemplateConfigurations #tier_count tier, with local config"(){
+    setup:
+    PipelineConfig pipelineConfigMain =  Mock(PipelineConfig)
+
+    TemplateConfigObject templateConfigObject = Mock(TemplateConfigObject)
+    //1 * templateConfigObject.asBoolean() >> { return true }
+
+    tier_count * pipelineConfigMain.join(templateConfigObject) >> { return }
+
+    def tiers = []
+
+    for( int t = 0; t < tier_count; t++ ) {
+      GovernanceTier tier = GroovyMock(GovernanceTier)
+      1 * tier.getConfig() >> { return templateConfigObject }
+      tiers << tier
+    }
+
+    GroovyMock(GovernanceTier, global: true)
+    1 * GovernanceTier.getHierarchy() >> { return tiers }
+    1 * GovernanceTier.getCONFIG_FILE() >> { return "pipeline_config.groovy" }
+
+    FileSystemWrapper fsw = GroovyMock(FileSystemWrapper, global: true)
+    1 * FileSystemWrapper.createFromJob() >> { return fsw }
+
+    TemplateConfigObject appTemplate = Mock(TemplateConfigObject)
+    String appTemplateContent = "_"
+    1 * fsw.getFileContents("pipeline_config.groovy", _, _) >> { return appTemplateContent }
+
+    GroovyMock(TemplateConfigDsl, global: true)
+    1 * TemplateConfigDsl.parse(appTemplateContent) >> { return appTemplate }
+    1 * pipelineConfigMain.join(appTemplate) >> { return }
+
+
+    when:
+    templateEntryPointVariable.aggregateTemplateConfigurations(pipelineConfigMain)
+
+    then:
+    true == true
+
+    where:
+    tier_count << [0, 1, 5]
 
   }
 }
