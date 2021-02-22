6a191bca15466c5706dee01a101140580bc2926b
rayvincent2 <rayhvincent@gmail.com>
2021-02-22 13:23:16 -0800

    Fix bug in how PluginLibraryProvider reads jar contents - #168 (#169)

--- src/main/groovy/org/boozallen/plugins/jte/init/governance/libs/PluginLibraryProvider.groovy
+++ src/main/groovy/org/boozallen/plugins/jte/init/governance/libs/PluginLibraryProvider.groovy
@@ -84,8 +84,8 @@ class PluginLibraryProvider extends LibraryProvider{
 
                 String thing = parts[2]
                 if(thing == CONFIG_FILE){
-                    libraries[libName] = getFileContents(zipFile, zipEntry)
-                } else if (thing in [ "steps", "resources"]){
+                    libraries[libName].config = getFileContents(zipFile, zipEntry)
+                } else if (thing in [ "steps", "resources"] && !path.endsWith('/')){
                     String relativePath = path - "libraries/${libName}/"
                     libraries[libName][thing][relativePath] = getFileContents(zipFile, zipEntry)
                 }
